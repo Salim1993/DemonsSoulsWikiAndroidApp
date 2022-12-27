c0c6046d1f45b762de351b87427a3f3130f522b7
DESKTOP-POL0H0E\Salim <salimbenkhaled93@gmail.com>
2022-12-27 12:34:59 -0700

    Got tests working
    Onedrive was causing issues.

--- app/src/test/java/com/example/demonsoulswiki/weapon/GetWeaponUseCaseTest.kt
+++ app/src/test/java/com/example/demonsoulswiki/weapon/GetWeaponUseCaseTest.kt
@@ -5,9 +5,8 @@ import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
 import io.mockk.coVerify
-import kotlinx.coroutines.flow.SharedFlow
+import io.mockk.impl.annotations.RelaxedMockK
 import kotlinx.coroutines.flow.emptyFlow
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
@@ -18,7 +17,7 @@ class GetWeaponUseCaseTest {
     @MockK
     lateinit var service: DarkSoulsApiService
 
-    @MockK
+    @RelaxedMockK
     lateinit var weaponDao: WeaponDao
 
     //System to test
@@ -28,18 +27,13 @@ class GetWeaponUseCaseTest {
     fun setup() {
         MockKAnnotations.init(this)
 
-        getWeaponUseCase = GetWeaponUseCase(service, weaponDao)
-
         coEvery { weaponDao.getAllWeapons() } returns emptyFlow()
+
+        getWeaponUseCase = GetWeaponUseCase(service, weaponDao)
     }
 
     @Test
-    fun test1() {
-        assert(true)
-    }
-
-    @Test
-    fun `get weapon info successfully and save to database`() = runTest(UnconfinedTestDispatcher()) {
+    fun `get weapon info successfully and save to database`() = runTest() {
 
         //arrange
         val weaponsList = listOf(
